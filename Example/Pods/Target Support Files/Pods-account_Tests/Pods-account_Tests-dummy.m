ffc2eac84f83febe020aae3b71879d6ce67a6cd5
yuxin.she <yuxin.she@anker-in.com>
2022-10-29 22:00:16 +0800

    init account

---
+++ Example/Pods/Target Support Files/Pods-account_Tests/Pods-account_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_account_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_account_Tests
+@end
---
+++ Example/Pods/Target Support Files/account/account-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_account : NSObject
+@end
+@implementation PodsDummy_account
+@end
